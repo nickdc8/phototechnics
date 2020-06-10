d8dc99f139384bdbc971f08fdcfeb53ca208329a
Nick C <nickdc8@gmail.com>
2020-06-10 09:33:24 +0100

    lang added

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "gatsby-plugin-html-attributes",
+      options: {
+        lang: "en",
+      },
+    },
+    {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
